325fb2c6ade928c4fb5444d95f9cf22993284987
Leandro Costa Silva <lcostasilva02@gmail.com>
2023-01-04 09:57:29 -0300

    Os motores do React - Inserindo comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,66 +1,94 @@
-import { format, formatDistanceToNow } from 'date-fns';
-import ptBR from 'date-fns/locale/pt-BR';
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+import { useState } from "react";
 
-import { Comment } from './Comment';
-import { Avatar } from './Avatar';
+import { Comment } from "./Comment";
+import { Avatar } from "./Avatar";
 
-import styles from './Post.module.css';
+import styles from "./Post.module.css";
 
 export function Post({ author, publishedAt, content }) {
+  const [comments, setComments] = useState([
+  ]);
 
-  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
-    locale: ptBR,
-  });
+  const [newCommentText, setNewCommentText] = useState('');
+
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'às' HH:mm'h'",
+    {
+      locale: ptBR,
+    }
+  );
 
   const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
     locale: ptBR,
-    addSuffix: true
+    addSuffix: true,
   });
+
+  function handleCrateNewComment() {
+    event.preventDefault();
+
+    setComments([...comments, newCommentText]);
+    setNewCommentText('');
+  }
+
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value);
+  }
 
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-        <Avatar src={author.avatarUrl} />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
             <strong>{author.name}</strong>
             <span>{author.role}</span>
           </div>
         </div>
 
-        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
           {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-      {content.map(line => {
-          if (line.type === 'paragraph') {
+        {content.map((line) => {
+          if (line.type === "paragraph") {
             return <p>{line.content}</p>;
-          } else if (line.type === 'link') {
-            return <p><a href="#">{line.content}</a></p>
+          } else if (line.type === "link") {
+            return (
+              <p>
+                <a href="#">{line.content}</a>
+              </p>
+            );
           }
         })}
       </div>
-      
-      <form className={styles.commentForm}>
+
+      <form onSubmit={handleCrateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
 
         <textarea
-          placeholder="Deixe um comentário"
-        />
+           name="comment"
+           value={newCommentText}
+           onChange={handleNewCommentChange}
+           placeholder="Deixe um comentário" />
 
         <footer>
           <button type="submit">Publicar</button>
         </footer>
       </form>
 
-      
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map((comment) => {
+          return <Comment content={comment} />;
+        })}
       </div>
     </article>
-  )
+  );
 }
